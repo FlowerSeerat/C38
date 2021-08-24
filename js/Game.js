0505cf2719ae3e55b988d886ef87772170abaef2
FlowerSeerat <seerat3208@gmail.com>
2021-08-24 08:26:23 -0400

    gameCamera

--- js/Game.js
+++ js/Game.js
@@ -46,7 +46,33 @@ class Game {
     if (allPlayers !== undefined) {
       image(track, 0, -height * 5, width, height * 6);
 
+      var index=0;
+      for(var plr in allPlayers) {
+        index=index+1;
+        var x = allPlayers[plr].positionX;
+        var y = height-allPlayers[plr].positionY;
+
+        cars[index-1].position.x=x;
+        cars[index-1].position.y=y;
+        if(index===player.index){
+          stroke(10);
+          fill("red");
+          ellipse(x,y,60,60);
+          camera.position.x=cars[index-1].position.x;
+          camera.position.y=cars[index-1].position.y;
+        }
+      }
+      this.handlePlayerControls();
       drawSprites();
     }
   }
+
+  handlePlayerControls() {
+    if(keyIsDown(UP_ARROW)) {
+      player.positionY += 10;
+      player.update();
+    }
+  }
+
+ 
 }
